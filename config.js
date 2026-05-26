8f5929a032c0106e833f05476e3dd66bf7a5e750
createdbynosca <leonora.walczynski@gmail.com>
2026-05-26 20:37:36 +0200

    Neues Google Sheet

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     
     // Google Sheet ID (aus der URL deines Sheets)
     // Beispiel: https://docs.google.com/spreadsheets/d/DIESE_ID_HIER/edit
-    GOOGLE_SHEET_ID: '1I_zxxXqYvVvA6kOiE5VmOKwuRBXF5wovD6HYfhXFKyc',
+    GOOGLE_SHEET_ID: '1C2VvJqMBzmKnfslRwHrBcdtxvDzu2cNsQqx-m4TDNHA',
     
     // Name des Tabellenblatts (unten in Google Sheets)
     SHEET_NAME: 'Links',
@@ -21,10 +21,16 @@ const CONFIG = {
     // Diese werden angezeigt wenn das Sheet nicht geladen werden kann
     FALLBACK_LINKS: [
         {
-            category: 'Plotter',
+            category: 'Plotter & Drucker',
             title: 'Cricut Maker 4 - Starter Paket',
             url: '#',
             image: 'images/placeholder.jpg'
+        },
+        {
+            category: 'Plotter & Drucker',
+            title: 'Drucker',
+            url: 'https://amzn.to/4dGLKHi',
+            image: 'images/printer.png'
         }
     ]
 };
